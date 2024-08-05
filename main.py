2feda8037da75f2a038620c195f89e40f13363d9
imKrishn <imkrishbis@gmail.com>
2024-08-05 19:00:52 +0530

    added testDB, flask run test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -11,6 +12,22 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
+db = SQLAlchemy(app)
+
+class testDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(64))
+    passhash = db.Column(db.String(256))
+
+with app.app_context():
+    db.create_all()
+
 @app.route("/")
 def test():
-    return "<p>Test Passed.</p>"
+    # test_db = testDB(username='sample_username', passhash='sample_passhash')
+    # db.session.add(test_db)
+    # db.session.commit()
+    test_db_get = testDB.query.get(1)
+
+    return "<p>Flask run test passed. <br><br> Username: {} <br> Password: {} <br> testDB passed.</p>".format(test_db_get.username, test_db_get.passhash)
