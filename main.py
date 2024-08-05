44dc5d88b544b8e0c012e76dd624337404af4dda
imKrishn <imkrishbis@gmail.com>
2024-08-05 22:59:04 +0530

    removed some unnecessary lines

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 
-load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
