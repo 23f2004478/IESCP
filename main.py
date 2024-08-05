9bf65f746ea46731cc0ce70ce383ce15e0540882
imKrishn <imkrishbis@gmail.com>
2024-08-05 18:48:26 +0530

    added .env

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from main import app
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+
 @app.route("/")
 def test():
     return "<p>Test Passed.</p>"
